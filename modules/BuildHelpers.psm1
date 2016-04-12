45431e67a61181efbc02456da0537b8b2bea4779
Anton Zimin <anton@saritasa.com>
2016-04-12 18:21:43 +0700

    Add Invoke-Environment function. #80235

--- modules/BuildHelpers.psm1
+++ modules/BuildHelpers.psm1
@@ -1,7 +1,7 @@
 ﻿function Initialize-BuildVariables()
 {
     $scriptPath = ($env:VS150COMNTOOLS, $env:VS140COMNTOOLS, $env:VS120COMNTOOLS, $env:VS110COMNTOOLS -ne $null)[0] + 'vsvars32.bat'
-    &"$PSScriptRoot\Invoke-Environment.ps1" "`"$scriptPath`""
+    Invoke-Environment $scriptPath
 }
 
 function Invoke-NugetRestore([string] $solutionPath)
@@ -22,5 +22,29 @@ function Invoke-SolutionBuild([string] $solutionPath, [string] $configuration)
     if ($LASTEXITCODE)
     {
         throw "Build failed."
+    }
+}
+
+# Based on Invoke-Environment script.
+# https://github.com/nightroman/PowerShelf/blob/master/Invoke-Environment.ps1
+# Copyright (c) 2012-2016 Roman Kuzmin
+# Apache License, Version 2.0
+function Invoke-Environment([string] $command, [switch] $output, [switch] $force)
+{
+    $stream = if ($output) { ($temp = [IO.Path]::GetTempFileName()) } else { 'nul' }
+    $operator = if ($force) {'&'} else {'&&'}
+
+    foreach($_ in cmd /c " `"$command`" > `"$stream`" 2>&1 $operator SET")
+    {   
+        if ($_ -match '^([^=]+)=(.*)')
+        {
+            [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+        }
+    }
+
+    if ($output)
+    {
+        Get-Content -LiteralPath $temp
+        Remove-Item -LiteralPath $temp
     }
 }
