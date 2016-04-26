6090381c79c165e8cea2d9e7eac963d4aa668dc7
Anton Zimin <anton@saritasa.com>
2016-04-26 16:28:47 +0700

    Add Update-AssemblyInfoFiles function. #80235

--- modules/Saritasa.Build.psm1
+++ modules/Saritasa.Build.psm1
@@ -48,3 +48,30 @@ function Invoke-Environment([string] $command, [switch] $output, [switch] $force
         Remove-Item -LiteralPath $temp
     }
 }
+
+# Update version numbers of AssemblyInfo.cs and AssemblyInfo.vb.
+# Based on SetVersion script.
+# http://www.luisrocha.net/2009/11/setting-assembly-version-with-windows.html
+# Copyright (c) 2009 Luis Rocha
+function Update-AssemblyInfoFiles([string] $version)
+{
+    $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $assemblyVersion = 'AssemblyVersion("' + $version + '")';
+    $fileVersion = 'AssemblyFileVersion("' + $version + '")';
+    
+    Get-ChildItem -r -Include AssemblyInfo.cs, AssemblyInfo.vb | ForEach-Object {
+        $filename = $_.Directory.ToString() + '\' + $_.Name
+        $filename + ' -> ' + $version
+        
+        # If you are using a source control that requires to check-out files before 
+        # modifying them, make sure to check-out the file here.
+        # For example, TFS will require the following command:
+        # tf checkout $filename
+    
+        (Get-Content $filename) | ForEach-Object {
+            % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
+            % {$_ -replace $fileVersionPattern, $fileVersion }
+        } | Set-Content $filename
+    }
+}
