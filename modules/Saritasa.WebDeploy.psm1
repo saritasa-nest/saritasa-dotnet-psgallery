1dd83871e759a8ba27ff85e5479c04059ed07854
Anton Zimin <anton@saritasa.com>
2016-04-26 16:06:04 +0700

    Introduce Start-AppPool, Stop-AppPool, Invoke-WebDeployment functions. #80235

--- modules/Saritasa.WebDeploy.psm1
+++ modules/Saritasa.WebDeploy.psm1
@@ -1,3 +1,10 @@
+$msdeployPath = "$env:ProgramFiles\IIS\Microsoft Web Deploy V3\"
+
+function Set-MsdeployPath([string] $path)
+{
+    $script:msdeployPath = $path
+}
+
 function Invoke-PackageBuild([string] $projectPath,
                              [string] $packagePath,
                              [string] $configuration = 'Release',
@@ -9,4 +16,25 @@ function Invoke-PackageBuild([string] $projectPath,
         '/p:IncludeSetAclProviderOnDestination=False', "/p:PrecompileBeforePublish=$precompile",
         "/p:Platform=$platform", "/p:PackageLocation=$packagePath")
     msbuild.exe $projectPath $allBuildParams $basicBuildParams $buildParams
+}
+
+function Start-AppPool([string] $serverHost, [string] $siteName, [string] $application)
+{
+    'Starting app pool...'
+    &"$msdeployPath\msdeploy.exe" '-verb:sync' '-source:recycleApp' "-dest:recycleApp=`"$siteName/$application`",recycleMode=`"StartAppPool`",computername=$serverHost"
+}
+
+function Stop-AppPool([string] $serverHost, [string] $siteName, [string] $application)
+{
+    'Stopping app pool...'
+    &"$msdeployPath\msdeploy.exe" '-verb:sync' '-source:recycleApp' "-dest:recycleApp=`"$siteName/$application`",recycleMode=`"StopAppPool`",computername=$serverHost"
+}
+
+function Invoke-WebDeployment([string] $packagePath, [string] $serverHost, [string] $siteName, [string] $application)
+{
+    "Deploying $packagePath to $serverHost/$application..."
+    &"$msdeployPath\msdeploy.exe" "-source:package=$packagePath" `
+        "-dest:auto,computerName=`"https://$serverHost:8172/msdeploy.axd?site=$siteName`",includeAcls=`"False`"" `
+        '-verb:sync' '-disableLink:AppPoolExtension' '-disableLink:ContentExtension' '-disableLink:CertificateExtension' `
+        '-allowUntrusted' "-setParam:name='IIS Web Application Name',value='$siteName/$application'"
 }
