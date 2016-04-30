8b9cf42ac60e4e84296429f2f4ed1b247ed1f211
Anton Zimin <anton@saritasa.com>
2016-04-30 18:52:02 +0700

    Add Install-WinrmHttps script.

---
+++ scripts/WinRM/Install-WinrmHttps.ps1
@@ -0,0 +1,63 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 3ccd77cd-d928-4e72-98fc-82e3417f3427
+
+.AUTHOR Anton Zimin
+
+.COMPANYNAME Saritasa
+
+.COPYRIGHT (c) 2016 Saritasa. All rights reserved.
+
+.TAGS WinRM
+
+.LICENSEURI https://raw.githubusercontent.com/dermeister0/PSGallery/master/LICENSE
+
+.PROJECTURI https://github.com/dermeister0/PSGallery
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Generates self-signed certificate or uses existing. Configures HTTPS listener for WinRM service. Opens 5986 port in firewall. 
+
+#> 
+Param([string] $certificateThumbprint)
+
+trap
+{
+    Write-Host 'FAILURE' -BackgroundColor Red
+    $_
+    $host.SetShouldExit(1)
+    exit
+}
+
+$hostname = $env:COMPUTERNAME
+
+if (!$certificateThumbprint)
+{
+    $certificateThumbprint = (New-SelfSignedCertificate -DnsName $hostname -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+    'New certificate is generated.'
+}
+
+New-Item -Path WSMan:\localhost\Listener -Address * -Transport HTTPS -Hostname $hostname `
+    -CertificateThumbprint $certificateThumbprint -Force
+
+New-NetFirewallRule -DisplayName 'Windows Remote Management (HTTPS-In)' `
+    -Direction Inbound -Action Allow -Protocol TCP -LocalPort 5986
+    
+Write-Host "`nWinRM is set up for host $hostname." -ForegroundColor Green
