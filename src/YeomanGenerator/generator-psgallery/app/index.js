b418abe10109a35c04e6676e8b5eb33c62e46483
Anton Zimin <anton@saritasa.com>
2016-10-28 18:24:27 +0700

    Ask for project type. Install Saritasa.WebDeploy for web projects.

--- src/YeomanGenerator/generator-psgallery/app/index.js
+++ src/YeomanGenerator/generator-psgallery/app/index.js
@@ -1,12 +1,30 @@
 var generators = require('yeoman-generator');
+var mkdirp = require('mkdirp');
 
 module.exports = generators.Base.extend({
     initializing: function () {
         console.log('PSGallery generator started.');
     },
+    prompting: function () {
+        return this.prompt([{
+            type: 'list',
+            name: 'projectType',
+            message: 'Project type',
+            choices: ['Web', 'Desktop']
+        }]).then(function (answers) {
+            this.projectType = answers.projectType;
+        }.bind(this));
+    },
     writing: function () {
+        var modulesPath = this.destinationPath('scripts/modules')
+        mkdirp.sync(modulesPath);
+
         this.fs.copy(this.templatePath('default.ps1'), this.destinationPath('default.ps1'));
         this.fs.copy(this.templatePath('Scripts/Saritasa.PsakeTasks.ps1'), this.destinationPath('Scripts/Saritasa.PsakeTasks.ps1'));
+
+        if (this.projectType === 'Web') {
+            this.spawnCommand('powershell', ['-Command', '&{ Save-Module Saritasa.WebDeploy -Path ' + modulesPath + ' }']);
+        }
     }
 });
 
