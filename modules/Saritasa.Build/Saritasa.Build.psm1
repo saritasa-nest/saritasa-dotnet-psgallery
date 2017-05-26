75f707373a0327f511e856fb5e76932a9b1b9016
Anton Zimin <anton@saritasa.com>
2017-05-26 18:07:16 +0700

    Remove NuGet version check since v4 is stable now #28

--- modules/Saritasa.Build/Saritasa.Build.psm1
+++ modules/Saritasa.Build/Saritasa.Build.psm1
@@ -21,14 +21,6 @@ function Install-NugetCli
         Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $nugetExePath
         Write-Information 'Done.'
     }
-
-    $nugetVersion = ((Get-Item $nugetExePath).VersionInfo.ProductVersion).Split('.')[0]
-    if ($nugetVersion -lt 4)
-    {
-        Write-Information 'Downloading nuget.exe...'
-        Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/v4.0.0/nuget.exe' -OutFile $nugetExePath
-        Write-Information 'Done.'
-    }
 }
 
 <#
