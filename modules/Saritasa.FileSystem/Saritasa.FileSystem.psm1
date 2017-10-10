5bc385d322139ae47517c41f179a7ef7b7a5746d
Anton Zimin <anton@saritasa.com>
2017-10-10 19:34:35 +0700

    Restore comments in FileSystem #60

--- modules/Saritasa.FileSystem/Saritasa.FileSystem.psm1
+++ modules/Saritasa.FileSystem/Saritasa.FileSystem.psm1
@@ -4,9 +4,17 @@ Set-StrictMode -Version Latest
 .SYNOPSIS
 Removes all empty directories under the given path.
 
+.NOTES
+Author: Daniel Schroeder
+http://blog.danskingdom.com/powershell-functions-to-delete-old-files-and-empty-directories/
+
 .EXAMPLE
 Remove-EmptyDirectories -Path "C:\SomePath\Temp" -DeletePathIfEmpty
 Delete all empty directories in the Temp folder, as well as the Temp folder itself if it is empty.
+
+.EXAMPLE
+-Remove-EmptyDirectories -Path "C:\SomePath\WithEmpty\Directories" -OnlyDeleteDirectoriesCreatedBeforeDate ([DateTime]::Parse("Jan 1, 2014 15:00:00"))
+Delete all empty directories created after Jan 1, 2014 3PM.
 
 .EXAMPLE
 Remove-EmptyDirectories C:\SomePath\WithEmpty\Directories -OnlyDeleteDirectoriesCreatedBeforeDate [DateTime]::Today.AddDays(-1)
@@ -20,21 +28,21 @@ function Remove-EmptyDirectories
     [CmdletBinding(SupportsShouldProcess = $true)]
     param
     (
-        # Path to folder to scan
+        # Path to folder to scan.
         [parameter(Mandatory = $true)]
         [ValidateScript({Test-Path $_})]
         [string] $Path,
-        # If set, the root folder (specified in Path variable) will also be removed if it complies the provided filters
+        # If set, the root folder (specified in Path variable) will also be removed if it complies the provided filters.
         [switch] $DeletePathIfEmpty,
-        # If specified, only directories created before the given date will be removed
+        # If specified, only directories created before the given date will be removed.
         [DateTime] $OnlyDeleteDirectoriesCreatedBeforeDate = [DateTime]::MaxValue,
-        # If specified, only directories last time modified before the given date will be removed
+        # If specified, only directories last time modified before the given date will be removed.
         [DateTime] $OnlyDeleteDirectoriesNotModifiedAfterDate = [DateTime]::MaxValue
     )
 
     Get-ChildItem -Path $Path -Recurse -Force -Directory |
-        Where-Object { $null -eq (Get-ChildItem -Path $_.FullName -Recurse -Force -File) } | 
-        Where-Object { $_.CreationTime -lt $OnlyDeleteDirectoriesCreatedBeforeDate -and $_.LastWriteTime -lt $OnlyDeleteDirectoriesNotModifiedAfterDate } | 
+        Where-Object { $null -eq (Get-ChildItem -Path $_.FullName -Recurse -Force -File) } |
+        Where-Object { $_.CreationTime -lt $OnlyDeleteDirectoriesCreatedBeforeDate -and $_.LastWriteTime -lt $OnlyDeleteDirectoriesNotModifiedAfterDate } |
         ForEach-Object `
         {
             Write-Verbose $_.FullName
@@ -57,6 +65,10 @@ function Remove-EmptyDirectories
 <#
 .SYNOPSIS
 Removes all files in the given Path that were created before the given date, as well as any empty directories that may be left behind.
+
+.NOTES
+Author: Daniel Schroeder
+http://blog.danskingdom.com/powershell-functions-to-delete-old-files-and-empty-directories/
 
 .EXAMPLE
 Remove-FilesCreatedBeforeDate -Path "C:\Some\Directory" -DateTime ((Get-Date).AddDays(-2)) -DeletePathIfEmpty
@@ -83,7 +95,7 @@ function Remove-FilesCreatedBeforeDate
         [switch] $DeletePathIfEmpty
     )
     Get-ChildItem -Path $Path -Recurse -Force -File |
-        Where-Object { $_.CreationTime -lt $DateTime } | 
+        Where-Object { $_.CreationTime -lt $DateTime } |
 		ForEach-Object `
         {
             Write-Verbose $_.FullName
@@ -95,6 +107,10 @@ function Remove-FilesCreatedBeforeDate
 <#
 .SYNOPSIS
 Removes all files in the given Path that have not been modified after the given date, as well as any empty directories that may be left behind.
+
+.NOTES
+Author: Daniel Schroeder
+http://blog.danskingdom.com/powershell-functions-to-delete-old-files-and-empty-directories/
 
 .EXAMPLE
 Remove-FilesNotModifiedAfterDate -Path "C:\Another\Directory" -DateTime ((Get-Date).AddHours(-8))
